8fb4bef9a818f099712652da8694849b7ef4cf1a
superdebug <ex_plus@163.com>
2016-11-23 02:39:23 -0500

    my  commits

---
+++ charts/readme.txt
@@ -0,0 +1,9 @@
+pip3 install charts
+安装好highchart后
+进入python3的安装目录内
+import sys
+print(sys.path)
+进入目录
+/usr/local/python3/lib/python3.5/site-packages/charts
+将本目录内的文件替换到安装目录下即可
+
---
+++ need-install-pip.sh
@@ -0,0 +1,21 @@
+sudo yum install openssl-devel
+pip3 install pyOpenSSL
+pip3 install twisted
+
+pip3 install requests
+pip3 install virtualenv
+
+pip3 install bs4
+pip3 install wheel    
+pip3 install Requests
+pip3 install lxml
+
+pip3 install pymongo
+pip3 install cryptography
+
+
+pip3 install jupyter
+pip3 install charts
+pip3 install PyMySQL
+pip3 install scrapy
+
