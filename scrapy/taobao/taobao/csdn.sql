0adcafbdb633fd660107045cf561e2e97835b320
superdebug <ex_plus@163.com>
2017-01-08 10:47:04 +0800

    my  commits

---
+++ scrapy/taobao/taobao/csdn.sql
@@ -0,0 +1,4 @@
+create database csdn DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+use csdn;
+create table taob (title varchar(100),link varchar(60) primary key,price float(30),comment int(30))ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
