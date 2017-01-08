ce285ca665ab6898b482ab3b298b35df237f01e1
superdebug <ex_plus@163.com>
2017-01-08 10:45:23 +0800

    my  commits

--- git-push.sh
+++ git-push.sh
@@ -4,5 +4,5 @@ git add *
 #git commit -a
 git commit -m "my  commits"
 git remote add origin git@github.com:superdebug/spider-exam.git
-#git push -u origin master
+git push -u origin master
 #git push -u origin +master
