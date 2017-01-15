b17334c7c40eed6dfa5840d57b9ed995da31ed33
superdebug <ex_plus@163.com>
2017-01-15 17:23:39 +0800

    my  commits

--- scrapy/huanqiu-daili/huanqiu/middlewares.py
+++ scrapy/huanqiu-daili/huanqiu/middlewares.py
@@ -7,6 +7,18 @@
 
 from scrapy import signals
 import base64
+import random
+class RandomUserAgent(object):
+    def __init__(self, agents):
+        self.agents = agents
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.getlist('USER_AGENTS'))
+    def process_request(self, request, spider):
+        request.headers.setdefault('User-Agent', random.choice(self.agents))
+        print('*******************当前User-Agent是*********************************')
+        print (request.headers['User-Agent'])
+
 
 
 class ProxyMiddleware(object):
