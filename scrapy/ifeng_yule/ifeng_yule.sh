bbeef6e0c512c93782a176c6c9b03900417282f3
superdebug <ex_plus@163.com>
2017-01-24 17:41:20 +0800

    my  commits

--- scrapy/ifeng_yule/ifeng_yule.sh
+++ scrapy/ifeng_yule/ifeng_yule.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-scrapy crawl yule --nolog
+#!/bin/sh 
+. ~/.bash_profile
+#export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
+cd /opt/spider-exam/scrapy/ifeng_yule
+/usr/local/bin/scrapy crawl yule --nolog
 
